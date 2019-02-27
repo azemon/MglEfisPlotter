31151106209934288c431941aa07ae4ece32f818
Art Zemon <art@zemon.name>
2019-02-27 07:04:25 -0600

    correct handling of records and improve handling of the data stream within the records. parse message types 1 and 2.

--- MglDataFile.py
+++ MglDataFile.py
@@ -1,6 +1,6 @@
 import datetime
 import struct
-from typing import BinaryIO
+from typing import BinaryIO, List
 
 """
 Functions to convert between Python values and C structs.
@@ -34,104 +34,221 @@ The variable struct.error is an exception raised on errors.
 """
 
 
-class DataError(Exception):
+class NotPartOfFlightException(Exception):
     pass
 
 
-class EOF(Exception):
+class NotAMessage(Exception):
     pass
 
 
-class Timestamp(object):
-    mglTimestamp = None
-    datetime = None
+class MessageData(object):
+    pass
 
-    mglEpoch = None
 
-    def __init__(self, mglTimestamp):
-        # self.mglEpoch = datetime.datetime(2003, 11, 30, 1, 6, 49)
-        self.mglEpoch = datetime.datetime(2000, 1, 1, 0, 0, 0)
-        self.mglTimestamp = mglTimestamp
-        self.calculateDatetime()
+class PrimaryFlight(MessageData):
+    pAltitude: int
+    bAltitude: int
+    asi: int
+    tas: int
+    aoa: int
+    vsi: int
+    baro: int
+    local: int
+    oat: int
+    humidity: int
+    systemFlags: int
+    hour: int
+    minute: int
+    second: int
+    date: int
+    month: int
+    year: int
+    ftHour: int
+    ftMin: int
 
-    def calculateDatetime(self):
-        sinceEpoch = datetime.timedelta(seconds=self.mglTimestamp)
-        self.datetime = self.mglEpoch + sinceEpoch
+    baroHg: float
+    oatF: float
+    dateTime: datetime
+
+    exception: Exception
+
+    def __init__(self, buffer: bytearray):
+        (self.pAltitude, self.bAltitude, # ii
+         self.asi, self.tas, # HH
+         self.aoa, self.vsi, # hh
+         self.baro, self.local, # HH
+         self.oat, self.humidity, # hb
+         self.systemFlags, # B
+         self.hour, self.minute, self.second, self.date, self.month, self.year, # bbbbbb
+         self.ftHour, self.ftMin, # bb
+         ) = struct.unpack('ii HH hh HH hb B bbbbbb bb', buffer)
+
+        self.baroHg = self.baro / 33.864 / 10
+        self.oatF = (self.oat + 40) * 9 / 5 - 40
+        try:
+            self.dateTime = datetime.datetime(self.year + 2000, self.month, self.date, self.hour, self.minute, self.second)
+        except ValueError as e:
+            self.exception = e
+
+
+class Gps(MessageData):
+    latitude: int
+    longitude: int
+    gpsAltitude: int
+    agl: int
+    northVelocity: int
+    eastVelocity: int
+    downVelocity: int
+    groundSpeed: int
+    trueTrack: int
+    variation: int
+    gps: int
+    satsTracked: int
+    satsVisible: int
+    horizontalAccuracy: int
+    verticalAccuracy: int
+    gpsCapability: int
+    raimStatus: int
+    raimHError: int
+    raimVError: int
+
+    latitudeDegrees: float
+    longitudeDegrees: float
+    groundSpeedKnots: float
+
+    def __init__(self, buffer: bytearray):
+        (self.latitude, self.longitude, # ii
+         self.gpsAltitude, self.agl, # ii
+         self.northVelocity, self.eastVelocity, self.downVelocity, # iii
+         self.groundSpeed, self.trueTrack, self.variation, # HHh
+         self.gps, self.satsTracked, self.satsVisible, # bbb
+         self.horizontalAccuracy, self.verticalAccuracy, self.gpsCapability, # bbb
+         self.raimStatus, self.raimHError, self.raimVError, # bbb
+         ) = struct.unpack_from('ii ii iii HHh bbb bbb bbb x', buffer)
+
+        self.latitudeDegrees = self.latitude / 180 / 1000
+        self.longitudeDegrees = self.longitude / 180 / 1000
+        self.groundSpeedKnots = self.groundSpeed * 0.1944
+
+
+
+class Message(object):
+    totalBytes: int
+    type: int
+    rate: int
+    count: int
+    version: int
+    data: bytearray
+    checksun: int
+
+    messageData: MessageData
+
+    def __init__(self, buffer: bytearray):
+        (dle, stx, length, lengthXor) = struct.unpack_from('BBBB', buffer)
+        calculatedLength = lengthXor ^ 0xff
+        if dle != 0x5 or stx != 0x2 or length != calculatedLength:
+            raise NotAMessage()
+        self.totalBytes = 4
+
+        (self.type, self.rate, self.count, self.version) = struct.unpack_from('BBBB', buffer, self.totalBytes)
+        self.totalBytes += 4
+
+        length += 8
+        format = '{length}s I'.format(length=length)
+        slice = buffer[self.totalBytes : self.totalBytes + length + 4]
+        sliceLen = len(slice)
+        (self.data, self.checksum) = struct.unpack(format, slice)
+        self.totalBytes += length + 4
+
+        self.setMessageData()
+        return
+
+    def setMessageData(self):
+        if 1 == self.type:
+            self.messageData = PrimaryFlight(self.data)
+        elif 2 == self.type:
+            self.messageData = Gps(self.data)
+
+
+class Flight(object):
+    earliestTimestamp: int
+    latestTimestamp: int
+    byteStream: bytearray
+    messages: List[Message]
+
+    NEWFLIGHTDELTA = 60
+
+    def __init__(self, timestamp: int, buffer: bytes):
+        self.earliestTimestamp = timestamp
+        self.latestTimestamp = timestamp
+        self.byteStream = bytearray(buffer)
+
+    def addBytes(self, timestamp: int, buffer: bytes):
+        if timestamp == 0 or timestamp > (self.latestTimestamp + self.NEWFLIGHTDELTA):
+            raise NotPartOfFlightException()
+        self.latestTimestamp = timestamp
+        self.byteStream.extend(buffer)
+
+    def analyze(self):
+        totalBytes = 0
+        self.messages = []
+        while totalBytes < len(self.byteStream):
+            message = Message(self.byteStream[totalBytes:])
+            self.messages.append(message)
+            totalBytes += message.totalBytes
+        return
 
     def __str__(self):
-        return str(self.datetime)
-
-    def __le__(self, other):
-        return self.datetime <= other
-
-    def __lt__(self, other):
-        return self.datetime < other
-
-    def __eq__(self, other):
-        return self.datetime == other
-
-    def __ne__(self, other):
-        return self.datetime != other
-
-    def __gt__(self, other):
-        return self.datetime > other
-
-    def __ge__(self, other):
-        return self.datetime >= other
+        return 'Flight from {earliest} to {latest} with {bytes} bytes'.format(earliest=self.earliestTimestamp,
+                                                                              latest=self.latestTimestamp,
+                                                                              bytes=len(self.byteStream))
 
 
-class MglRecord(object):
+class Packet(object):
+    timestamp: int
+    buffer: bytearray
+
+    def __init__(self, timestamp: int, buffer: bytearray):
+        self.timestamp = timestamp
+        self.buffer = buffer
+
+
+class MglPacketStream(object):
     filePointer: BinaryIO
+    packets: List[Packet]
+    flights: List[Flight]
 
-    rawRecord = None
-    rawDatastream = None
-
-    timeStamp = None
-
-    checksum = None
-    dle = None
-    messageCount = None
-    messageLength = None
-    messageType = None
-    stx = None
-
-    HEADERSIZE = 8
-    PRIVATEHEADERSIZE = 8
     RECORDSIZE = 512
 
-    def __init__(self, fp):
+    def __init__(self, fp: BinaryIO):
         self.filePointer = fp
+        self.packets = []
+        self.flights = []
+        self.readPackets()
+        self.createFlights()
 
-    def readRecord(self):
-        self.rawRecord = self.read(self.RECORDSIZE)
-        self.readTimestamp()
-        self.readDataStream()
-        self.parseHeader()
+    def createFlights(self):
+        flight = None
+        for packet in self.packets:
+            if flight is None:
+                flight = Flight(packet.timestamp, packet.buffer)
+            else:
+                try:
+                    flight.addBytes(packet.timestamp, packet.buffer)
+                except NotPartOfFlightException:
+                    self.flights.append(flight)
+                    flight = Flight(packet.timestamp, packet.buffer)
+        self.flights.append(flight)
 
-    def readDataStream(self):
-        self.rawDatastream = self.rawRecord[4:]
-
-    def readTimestamp(self):
-        (ts,) = struct.unpack_from('I', self.rawRecord, 0)
-        self.timeStamp = Timestamp(ts)
-
-    def readChecksum(self):
-        buffer = self.read(4)
-        (self.checksum,) = struct.unpack('I', buffer)
-
-    def parseHeader(self):
-        (self.dle, self.stx, self.messageLength, self.messageType, self.messageCount) = \
-            struct.unpack('BBBxBxBx', self.rawDatastream[0:self.HEADERSIZE])
-
-    def readMessageData(self):
-        length = self.messageLength + self.PRIVATEHEADERSIZE
-        self.rawMessageWithChecksum = self.rawDatastream[8:length]
-
-    def read(self, length):
-        buffer = self.filePointer.read(length)
-        if 0 == len(buffer):
-            raise EOF()
-        return buffer
+    def readPackets(self):
+        while True:
+            buffer = self.filePointer.read(self.RECORDSIZE)
+            if 0 == len(buffer):
+                return
+            (ts, buf) = struct.unpack_from('I 508s', buffer)
+            if 0 != ts:
+                self.packets.append(Packet(ts, bytearray(buf)))
 
 
 if '__main__' == __name__:
@@ -141,27 +258,12 @@ if '__main__' == __name__:
     first = maxDate
     last = minDate
 
-    try:
-        with open('data/IEFISBB.DAT', 'rb') as filePointer:
-            recordNumber = 0
-            print('%4s    %9s   %-19s   %4s    dle    stx    len' % (' Rec', 'Timestamp', 'DateTime', 'Type'))
-            while True:
-                m = MglRecord(filePointer)
-                m.readRecord()
-                recordNumber += 1
+    with open('data/IEFISBB.DAT', 'rb') as filePointer:
+        packetStream = MglPacketStream(filePointer)
+    print(packetStream)
+    print(packetStream.flights)
 
-                if (minDate <= m.timeStamp <= maxDate) or recordNumber <= 10:
-                    print('%4d    %9d   %19s   %4d    %3d    %3d    %3d' % (recordNumber, m.timeStamp.mglTimestamp, m.timeStamp, m.messageType, m.dle, m.stx, m.messageLength))
-
-                    if m.timeStamp < first:
-                        first = m.timeStamp
-
-                    if m.timeStamp > last:
-                        last = m.timeStamp
-
-    except EOF as e:
-        print()
-
-    print('first =', first)
-    print('last  =', last)
-    print('elapsed =', last.datetime - first.datetime)
+    for flight in packetStream.flights:
+        flight.analyze()
+        print(flight)
+        print(flight.messages)
