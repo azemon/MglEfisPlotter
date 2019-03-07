36ef7ea506656376fbff3db8a290ba44628f0bae
Art Zemon <art@zemon.name>
2019-03-07 04:05:38 +0000

    increase graph size and create flight1.ipynb

--- MglEfisPlotter/Plot.py
+++ MglEfisPlotter/Plot.py
@@ -8,7 +8,11 @@ from .Flight import Flight
 class Plot(object):
     flight: Flight
     colors: cycler
-
+        
+    dpi = 120
+    figsize = (10, 7)
+    fontsize = 12
+    
     def __init__(self, flight: Flight):
         self.flight = flight
         self.colors = plt.rcParams['axes.prop_cycle'].by_key()['color']
@@ -17,25 +21,28 @@ class Plot(object):
         self.flight.listAttributes()
 
     def plot(self, attr: str, label: str = None):
+        plt.figure(figsize=self.figsize, dpi=self.dpi)
         data = self.flight.getData(attr)
         if label is None:
             label = attr
         plt.plot(data.keys(), data.values(), label=label)
-        plt.ylabel(label)
+        plt.ylabel(label, fontsize=self.fontsize)
 
     def plot2(self, attr: List[str]):
         for i in range(0, len(attr)):
             if 0 == i:
-                fig, axis0 = plt.subplots()
+                fig, axis0 = plt.subplots(figsize=self.figsize, dpi=self.dpi)
                 axis = axis0
                 axis0.set_xlabel('Minutes')
             else:
                 axis = axis0.twinx()
+                offset = 1 + ((i - 1) * 0.15)
+                axis.spines['right'].set_position(('axes', offset))
 
-            axis.set_ylabel(attr[i], color=self.colors[i])
+            axis.set_ylabel(attr[i], color=self.colors[i], fontsize=self.fontsize)
             data = self.flight.getData(attr[i])
             axis.plot(data.keys(), data.values(), color=self.colors[i])
-
+    
     def save(self, fname: str, *args, **kwargs):
         self._addDecorations()
         plt.savefig(fname, *args, **kwargs)
@@ -46,4 +53,4 @@ class Plot(object):
 
     def _addDecorations(self):
         plt.title(self.flight.title())
-        plt.xlabel('Minutes')
+        plt.xlabel('Minutes', fontsize=self.fontsize)
